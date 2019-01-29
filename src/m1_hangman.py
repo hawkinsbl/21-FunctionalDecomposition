46ef8dd1f9d4835c4785b60c22a9b9d02b396b2f
hawkinbl <hawkinbl@rose-hulman.edu>
2019-01-28 20:02:02 -0500

    Commit

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -4,7 +4,7 @@ Hangman.
 Authors: Ben Hawkins and John Neil.
 """  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+# Done: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
 
